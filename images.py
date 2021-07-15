a2e6f3b476dda956b1f57a897cae78e46e378bac
krsecurity <79098696+krsecurity@users.noreply.github.com>
2021-07-15 08:50:31 +0100

    Add files via upload

---
+++ images.py
@@ -0,0 +1,17 @@
+import requests # to get image from the web
+import shutil # to save it locally
+key = 'XXXXXXXXX'
+for number in range(193420,215884):
+    image_url = "https://api.iconnectdaily.net/api/v1/media/" + str(number) + "?key=" + key + "&size=full&u=2021-07-14T09:32:29"
+    filename = image_url.split("/")[-1]
+    r = requests.get(image_url, stream = True)
+
+    if r.status_code == 200:
+        r.raw.decode_content = True
+    
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        
+        print('Image sucessfully downloaded: ',filename)
+    else:
+        print('Image couldn\'t be retreived')
